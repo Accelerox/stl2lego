fabca14a4ce78232c8f35517e282706dea1f1191
Mats Gard <matga917@student.liu.se>
2023-05-28 23:05:36 +0200

    Created a instantiation of JSON file to Catia using VB

---
+++ Catia/JSON2LEGO/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("JSON2LEGO")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Linköpings universitet")>
+<Assembly: AssemblyProduct("JSON2LEGO")>
+<Assembly: AssemblyCopyright("Copyright © Linköpings universitet 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("720e672b-b638-40a5-a329-0547cd7aafaa")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
